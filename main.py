7c266a2a2fb5c4c133d39f6ed42bc2e099cfae74
chananon-n <91945879+chananon-n@users.noreply.github.com>
2025-05-15 18:42:54 +0700

    detection 3 objects

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Create a new YOLO model from scratch
 model = YOLO("yolov8n.yaml")
 
-results = model.train(data="config.yaml", epochs=100)
+results = model.train(data="config.yaml", epochs=100,patience=20)
